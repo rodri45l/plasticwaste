0f87b62a5e4c4b995912173d16012755a3d1524d
Rodrigo <31670836+rodri45l@users.noreply.github.com>
2020-12-05 16:42:26 +0100

    Add files via upload

--- Trabajo1/tr1.R
+++ Trabajo1/tr1.R
@@ -18,14 +18,15 @@ df2[ df2 == "." ] <- NA
              
 df4 <- df3 %>% rename(country = `Carbon.Dioxide.emissions.from.fossil.fuel.consumption.and.cement.production.by.nation`,
   year = X2,
-  `Total CO2 emissions from fossil-fuels and cement production (thousand metric tons of C)` = X3,
-`Emissions from solid fuel consumption` = X4,
-`Emissions from liquid fuel consumption` = X5,
-`Emissions from gas fuel consumption` = X6,
-`Emissions from cement production` = X7,
-`Emissions from gas flaring` = X8,
-`Per capita CO2 emissions (metric tons of carbon)` = X9,
-`Emissions from bunker fuels` = X10
+   total_co2 = X3 #`Total CO2 emissions from fossil-fuels and cement production (thousand metric tons of C)`
+   ,
+em_sf = X4, #`Emissions from solid fuel consumption` 
+em_lf = X5, #`Emissions from liquid fuel consumption` 
+em_gf = X6, #`Emissions from gas fuel consumption` 
+em_cp = X7, #`Emissions from cement production` ,
+em_gfl = X8, #`Emissions from gas flaring`
+em_c02_pc = X9, #`Per capita CO2 emissions (metric tons of carbon)`
+em_bf = X10 #`Emissions from bunker fuels`
 ) 
   
   df5 <-df4 %>% group_by(country)
@@ -33,9 +34,34 @@ df4 <- df3 %>% rename(country = `Carbon.Dioxide.emissions.from.fossil.fuel.consu
     
     
   g<-  df5 %>% ggplot() +
-    geom_line(aes(year,`Total CO2 emissions from fossil-fuels and cement production (thousand metric tons of C)` ,color = country)) +
+    geom_line(aes(year,total_co2 ,color = country)) +
       theme(legend.position="none")
    
   
    ggplotly(g)
+   
+   
+   
+w_df <- df4 %>% group_by(year) %>% summarise(n=sum(total_co2))%>% ungroup()
+
+  g<-  w_df %>% ggplot() +
+    geom_point(aes(year,n)) +
+     geom_line(aes(year,n)) +
+     geom_smooth(aes(year,n,color= "red"))
+  g
+  
+  
+  n_df <- df4 %>% group_by(country) %>% summarise(total_co2=sum(total_co2))%>% ungroup() %>% filter(country= c)
+c <- c("UNITED KINGDOM","SERBIA")
+  n<-  n_df %>% ggplot() +
+    geom_histogram(aes(total_co2)) +
+     geom_line(aes(country,total_co2)) +
+     geom_smooth(aes(country,n,color= "red"))
+  n
+  
+  
+  
+  
+  
+  
     
